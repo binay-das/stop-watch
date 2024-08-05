51a066689d3c71680459432b3935e424789cd912
Binay <binaydas1123@gmail.com>
2024-08-06 03:03:28 +0530

    Files added

---
+++ script.js
@@ -0,0 +1,71 @@
+let start = document.getElementById("start");
+let stop = document.getElementById("stop");
+let reset = document.getElementById("reset");
+
+let timer = document.getElementById("timer");
+
+let msec = 0;
+let sec = 0;
+let mins = 0;
+
+let timerId = null;
+
+let startTimer = () => {
+    msec++;
+
+    if(msec == 100){
+        msec = 0;
+        sec++;
+    }
+    if(sec == 60){
+        sec = 0;
+        mins++;
+    }
+
+    let msecString;
+    let secString;
+    let minsString;
+    if(msec < 10){
+        msecString = `0${msec}`;
+    }
+    else {
+        msecString = msec;
+    }
+
+    if(sec < 10){
+        secString = `0${sec}`;
+    }
+    else {
+        secString = sec;
+    }
+
+    if(mins < 10){
+        minsString = `0${mins}`;
+    }
+    else {
+        minsString = mins;
+    }
+
+
+    timer.innerHTML = `${minsString} : ${sec} : ${msecString}`;
+}
+
+start.addEventListener("click", function(){
+    if(timerId != null){
+        clearInterval(timerId);
+    }
+    timerId = setInterval(startTimer, 10);
+});
+
+stop.addEventListener("click", function(){
+    clearInterval(timerId);
+});
+
+reset.addEventListener("click", function(){
+    clearInterval(timerId);
+    timer.innerHTML = `00 : 00 : 00`;
+
+    msec = sec = mins = 0;
+});
+
+
